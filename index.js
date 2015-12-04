35594afc56faee43e8faf60aa33cb1bfc644600b
mguida22 <michael.guida@colorado.edu>
2015-12-04 13:23:23 -0700

    queue it up

--- index.js
+++ index.js
@@ -12,54 +12,60 @@ var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-d3.json("data/us.json", function(err, us) {
+function display(us, airports, delays) {
+  var r, proj;
+
+  svg.insert("path", ".graticule")
+    .datum(topojson.feature(us, us.objects.land))
+    .attr("class", "land")
+    .attr("d", path);
+
+  svg.insert("path", ".graticule")
+    .datum(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000); }))
+    .attr("class", "county-boundary")
+    .attr("d", path);
+
+  svg.insert("path", ".graticule")
+    .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+    .attr("class", "state-boundary")
+    .attr("d", path);
+
+  svg.append("svg:g")
+    .attr("id", "circles")
+    .selectAll("circle")
+      .data(airports)
+    .enter().append("svg:circle")
+      .attr('transform', function (d) {
+        proj = projection([d.Longitude * -1, d.Latitude]);
+        if (proj !== null) {
+          return 'translate(' +
+            proj +
+          ')';
+        }
+        return null;
+      })
+      .attr("r", function(d) {
+        if (delays[d.locationID] > 0) {
+          r = delays[d.locationID] * 0.3;
+          if (r !== null) {
+            return r;
+          }
+        }
+        return 0;
+      });
+}
+
+function ready(err, us, airports, delays) {
   if (err) {
     return console.error(err);
   }
 
-  d3.csv("data/airport-codes.csv", function(err, airports) {
-    if (err) {
-      return console.error(err);
-    }
+  delays = delays['2008-01-01'];
+  display(us, airports, delays);
+}
 
-    d3.json("data/delays.json", function(err, delays) {
-      if (err) {
-        return console.error(err);
-      }
-
-      delays = delays['2008-01-01'];
-
-      svg.insert("path", ".graticule")
-        .datum(topojson.feature(us, us.objects.land))
-        .attr("class", "land")
-        .attr("d", path);
-
-      svg.insert("path", ".graticule")
-        .datum(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000); }))
-        .attr("class", "county-boundary")
-        .attr("d", path);
-
-      svg.insert("path", ".graticule")
-        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-        .attr("class", "state-boundary")
-        .attr("d", path);
-
-      svg.append("svg:g")
-        .attr("id", "circles")
-        .selectAll("circle")
-          .data(airports)
-        .enter().append("svg:circle")
-          .attr('transform', function (d) {
-            return 'translate(' +
-              projection([d.Longitude * -1, d.Latitude]) +
-            ')';
-          })
-          .attr("r", function(d) {
-            if (delays[d.locationID] > 0) {
-              return delays[d.locationID] * 0.3;
-            }
-            return 0;
-          });
-    });
-  });
-});
+queue()
+  .defer(d3.json, "data/us.json")
+  .defer(d3.csv, "data/airport-codes.csv")
+  .defer(d3.json, "data/delays.json")
+  .await(ready);
