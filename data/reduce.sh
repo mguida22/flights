34a285f64e284a60a9037af8e23d87134ecdc133
mguida22 <michael.guida@colorado.edu>
2015-12-03 22:58:07 -0700

    remove trailing space

--- data/reduce.sh
+++ data/reduce.sh
@@ -9,7 +9,7 @@ touch $OUTFILE
 
 # Grab important columns and combine YYYY-MM-DD into the proper format
 awk -v outfile="$OUTFILE" -F "\"*,\"*" '{
-  printf "%d%s%02d%s%02d %d %s \n", $1, "-", $2, "-", $3, $16, $17, $18 > outfile
+  printf "%d%s%02d%s%02d %d %s\n", $1, "-", $2, "-", $3, $16, $17, $18 > outfile
 }' $INFILE
 
 echo "Finished."
