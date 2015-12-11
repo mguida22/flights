5b3db0d3a2b3935f2117a6dd3593b74a0687f247
mguida22 <michael.guida@colorado.edu>
2015-12-11 16:18:04 -0700

    combine scripts, format date properly

--- data/delays.py
+++ data/delays.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import csv
 import json
@@ -6,23 +6,44 @@ import json
 sums = {}
 counts = {}
 
+print('Reducing data. This may take a long time.')
+
+outfile = open('2008-flights.csv', 'w')
+with open('2008.csv', newline='') as infile:
+    reader = csv.reader(infile, delimiter=',')
+    for row in reader:
+        outfile.write(row[0]+"-"+row[1]+"-"+row[2]+" "+row[15]+" "+row[16]+"\n")
+
+outfile.close()
+
+print("Finished.")
 print('Averaging delay data by date for each airport.')
 
 with open('2008-flights.csv', newline='') as f:
+    next(f)
     reader = csv.reader(f, delimiter=' ')
     for row in reader:
         if row[0] in sums:
             if row[2] in sums[row[0]]:
-                counts[row[0]][row[2]] += 1
-                sums[row[0]][row[2]] += int(row[1])
+                try:
+                    sums[row[0]][row[2]] += int(row[1])
+                    counts[row[0]][row[2]] += 1
+                except:
+                    pass
             else:
-                counts[row[0]][row[2]] = 1
-                sums[row[0]][row[2]] = int(row[1])
+                try:
+                    sums[row[0]][row[2]] = int(row[1])
+                    counts[row[0]][row[2]] = 1
+                except:
+                    pass
         else:
-            counts[row[0]] = {}
-            counts[row[0]][row[2]] = 1
-            sums[row[0]] = {}
-            sums[row[0]][row[2]] = int(row[1])
+            try:
+                sums[row[0]] = {}
+                sums[row[0]][row[2]] = int(row[1])
+                counts[row[0]] = {}
+                counts[row[0]][row[2]] = 1
+            except:
+                pass
 
 for date in sums:
     for airport in sums[date]:
